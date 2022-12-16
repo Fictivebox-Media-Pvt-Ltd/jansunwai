214505d6f2917b42c258c9e1599c5d7ba87e4998
deepak-fictivebox <deepak@fictivebox.com>
2022-12-16 14:07:51 +0530

    group user list api modified

--- api/get_surveyor_details.php
+++ api/get_surveyor_details.php
@@ -15,24 +15,25 @@ if($jwt){
             ));
             exit();
     }
+
     $todaydate = date('Y-m-d',strtotime("-1 days"));
       $user_id = $_POST['user_id'];
       $voter="SELECT  DISTINCT voter_id FROM tbl_survey WHERE surveyed_by = '$user_id' AND created_at >'$todaydate'";
       $result=mysqli_query($conn,$voter) or die("Query problem".mysqli_error($conn));
       $row = mysqli_num_rows($result);
-if(!empty($_POST['start_date']) && !empty($_POST['end_date'])){
+    if(!empty($_POST['start_date']) && !empty($_POST['end_date'])){
+      $startDate = $_POST['start_date'];
+      $endDate = $_POST['end_date'];
+      $voter="SELECT  DISTINCT voter_id FROM tbl_survey WHERE surveyed_by = '$user_id' AND  created_at BETWEEN '$endDate' AND '$startDate'";
+      $result=mysqli_query($conn,$voter) or die("Query problem".mysqli_error($conn));
+      $allTimeSurvey = mysqli_num_rows($result);
 
-  $voter="SELECT  DISTINCT voter_id FROM tbl_survey WHERE surveyed_by = '$user_id' AND ";
-  $result=mysqli_query($conn,$voter) or die("Query problem".mysqli_error($conn));
-  $allTimeSurvey = mysqli_num_rows($result);
-
-}
+    }
 else{
   $voter="SELECT  DISTINCT voter_id FROM tbl_survey WHERE surveyed_by = '$user_id'";
   $result=mysqli_query($conn,$voter) or die("Query problem".mysqli_error($conn));
   $allTimeSurvey = mysqli_num_rows($result);
 }
-
 
        if ($row > 0) {
           echo json_encode(array(
