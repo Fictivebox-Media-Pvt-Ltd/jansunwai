35bf3dfe4958f2ee88c181f90a35eaa9e67a9b0f
deepak-fictivebox <deepak@fictivebox.com>
2022-12-21 16:16:34 +0530

    update JWT token API

---
+++ api/get_surveyor_graph.php
@@ -0,0 +1,61 @@
+<?php
+include_once '../configs/database.php';
+require "../configs/jwtAuth.php";
+require "../vendor/autoload.php";
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+if($jwt){ 
+			try {
+			$decoded = JWT::decode($jwt,new Key($secret_key, 'HS256'));
+
+    if(empty($_POST['user_id'])){
+        echo json_encode(array(
+            "message" => "user_id field is required."
+            ));
+            exit();
+    }
+
+    $todaydate = date('Y-m-d',strtotime("-1 days"));
+      $user_id = $_POST['user_id'];
+      $voter="SELECT  DISTINCT voter_id FROM tbl_survey WHERE surveyed_by = '$user_id' AND created_at >'$todaydate'";
+      $result=mysqli_query($conn,$voter) or die("Query problem".mysqli_error($conn));
+      $row = mysqli_num_rows($result);
+      $graphQuery = "SELECT date_format(created_at,'%H %p') as hour,count(voter_id) as totalsurvey FROM tbl_survey group by date_format(created_at,'%H %p')";
+      //$voter="SELECT  DISTINCT voter_id FROM tbl_survey WHERE surveyed_by = '$user_id' ";
+      $graphresult=mysqli_query($conn,$graphQuery) or die("Query problem".mysqli_error($conn));
+      $row = mysqli_fetch_array($graphresult);
+      print_r($row);
+      die;
+
+      $info = array();
+      while ($row = mysqli_fetch_array($graphresult)){
+          $i++;
+          array_push($info, array(
+              'id' => $row['id'], 'question' => $row['question'], 'option1' => $row['option1'], 'option2' => $row['option2'], 'option3' => $row['option3'], 'option4' => $row['option4'],'option5' => $row['option5'],'option6' => $row['option6'],'option7' => $row['option7'],'option8' => $row['option8'],'option9' => $row['option9'],'option10' => $row['option10']));
+      }
+
+
+       if ($row > 0) {
+          echo json_encode(array(
+            "success" => true,
+            "message" => "Surveyor details",
+            "todaySurvey" => $row,
+            "allTimeSurvey" => $allTimeSurvey), JSON_UNESCAPED_UNICODE);
+      }
+      else{        
+        echo json_encode(array(
+          "success" => true,
+          "message" => "Data Not Found"), JSON_UNESCAPED_UNICODE);
+      }
+      }
+      catch (Exception $e){
+        http_response_code(401);
+        echo json_encode(array(
+        "success" => false,
+        "message" => "Access denied.",
+        "error" => $e->getMessage()
+        ));
+      }
+  }
+?>
---
+++ api/refresh_jwt_token.php
@@ -0,0 +1,74 @@
+<?php 
+include_once '../configs/database.php';
+require "../vendor/autoload.php";
+use Firebase\JWT\JWT;
+header("Access-Control-Allow-Origin: * ");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+ $response = array();
+ $user_id = $_POST['user_id'];
+ $voter="SELECT username FROM tbl_admin_users WHERE id = '$user_id'";
+ $result=mysqli_query($conn,$voter) or die("Query problem".mysqli_error($conn));
+ $row = mysqli_num_rows($result);
+ if ($row > 0) {
+ $row = mysqli_fetch_array($result);
+
+ $username = $row['username'];
+ $surveyed_by = $row['username'];
+ $stmt = $conn->prepare("SELECT id,username, f_name, l_name,aadhar_no,phone_no,email,user_image,(select sum(data.total_survey) as total_survey from ( select count(*) as total_survey from tbl_voter_survey where surveyed_by = ? UNION ALL select count(*) as total_survey from tbl_mumbai_voter_survey where surveyed_by = ? ) data), assigned_loksabha, assigned_vidhansabha FROM tbl_admin_users WHERE username = ?");
+ $stmt->bind_param("sss",$surveyed_by,$surveyed_by,$username);
+ $stmt->execute();
+ $stmt->store_result();
+ 
+ if($stmt->num_rows > 0){
+ $stmt->bind_result($id, $username, $f_name,$l_name,$aadhar,$phone,$email,$user_image,$total_survey,$assigned_loksabha,$assigned_vidhansabha);
+ $stmt->fetch();
+ $user = array(
+ 'id'=>$id, 
+ 'username'=>$username, 
+ 'name'=>$f_name." ".$l_name, 
+ 'aadhar_no'=>$aadhar,
+ 'phone_no'=>$phone,
+ 'email'=>$email,
+ 'user_image'=>$user_image,
+ 'total_survey'=>$total_survey,
+ 'assigned_loksabha'=>$assigned_loksabha,
+ 'assigned_vidhansabha'=>$assigned_vidhansabha
+ );
+
+$secret_Key  = '68V0zWFrS72GbpPreidkQFLfj4v9m3Ti+DXc8OB0gcM=';
+$date   = new DateTimeImmutable();
+$expire_at     = $date->modify('+2880 minutes')->getTimestamp();      // Add 60 seconds
+$domainName = "https://surveyapp.fictivebox.com";                  // Retrieved from filtered POST data
+$request_data = [
+    'iat'  => $date->getTimestamp(),         // Issued at: time when the token was generated
+    'iss'  => $domainName,                       // Issuer
+    'nbf'  => $date->getTimestamp(),         // Not before
+    'exp'  => $expire_at,                           // Expire
+    'data' => [
+        'uid' => $id,
+        'name' => $username,
+        'email' => $email]
+    ];
+
+     http_response_code(200);
+     $jwt = JWT::encode($request_data,$secret_Key,'HS256');
+ echo json_encode(
+         array(
+            "success" => true,
+             "message" => "token updated",
+             "jwt" => $jwt,
+             "expireAt" => $expire_at,
+             "user" => $user
+          ));
+ }
+}
+ else{
+
+     http_response_code(401);
+     echo json_encode(array( "success" => false,"message" => "failed"));
+ }
+//echo json_encode($response);
