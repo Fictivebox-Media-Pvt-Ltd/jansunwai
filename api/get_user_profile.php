8b9d6affc7ed3fbe70f172bed50789ce2d6775bf
deepak-fictivebox <deepak@fictivebox.com>
2022-12-27 11:42:39 +0530

    update or delete certificate API

--- api/get_user_profile.php
+++ api/get_user_profile.php
@@ -16,7 +16,7 @@ if($jwt){
             exit();
     }
       $user_id = $_POST['user_id'];
-      $voter="SELECT id,file_id,f_name,l_name,age,aadhar_no,phone_no,user_image,aadhar_front,aadhar_back FROM tbl_admin_users WHERE id = '$user_id'";
+      $voter="SELECT id,f_name,l_name,age,aadhar_no,phone_no,user_image,aadhar_front,aadhar_back FROM tbl_admin_users WHERE id = '$user_id'";
       $result=mysqli_query($conn,$voter) or die("Query problem".mysqli_error($conn));
       $row = mysqli_num_rows($result);
     
@@ -24,7 +24,7 @@ if($jwt){
         $personal_details = array();
           while ($row = mysqli_fetch_array($result)){
              array_push($personal_details, array(
-                  'user_id' => $row['id'],'file_id' => $row['file_id'], 'f_name' => $row['f_name'], 'l_name' => $row['l_name'], 'age' => $row['age'], 'aadhar_no' => $row['aadhar_no'], 'phone_no' => $row['phone_no'], 'user_image' => 'admin/images/avatar/'.$row['user_image'],'aadhar_front' => $row['aadhar_front'],'aadhar_back' => $row['aadhar_back']));
+                  'user_id' => $row['id'],'f_name' => $row['f_name'], 'l_name' => $row['l_name'], 'age' => $row['age'], 'aadhar_no' => $row['aadhar_no'], 'phone_no' => $row['phone_no'], 'user_image' => 'admin/images/avatar/'.$row['user_image'],'aadhar_front' => $row['aadhar_front'],'aadhar_back' => $row['aadhar_back']));
           }
 
           echo json_encode(array(
